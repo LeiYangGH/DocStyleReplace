5e268643cb75007d4ee96c635cc73223afd7850e
Lei, Yang <yang_lei@mcafee.com>
2017-08-08 11:18:56 +0800

    converted all to plain text

---
+++ DocReplace/DocReplace/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+namespace DocReplace
+{
+    class Program
+    {
+        static void SaveAsText(string fullName)
+        {
+            object oMissing = System.Reflection.Missing.Value;
+            Word._Application app;
+            Word._Document doc;
+            app = new Word.Application();
+            doc = app.Documents.Open(fullName);
+            string text = doc.Content.Text;
+            string txtFileName = fullName.Replace(@".doc", @".txt");
+            File.WriteAllText(txtFileName, text);
+            app.Quit();
+            Console.WriteLine(txtFileName);
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (string docFullName in Directory.GetFiles(@"C:\DocReplace\Old", "*.doc"))
+            {
+                SaveAsText(docFullName);
+            }
+            Console.WriteLine("ok");
+            Console.ReadLine();
+        }
+    }
+}
