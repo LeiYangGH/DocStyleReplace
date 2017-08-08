857d32fd4bc7210c866906a6d75a6413c3ebaea2
Lei, Yang <yang_lei@mcafee.com>
2017-08-08 18:39:29 +0800

    change all headers

--- ChangeHeader.bas
+++ ChangeHeader.bas
@@ -2,12 +2,14 @@ Sub ChangeHeader()
  Dim doc As Document, myFile As String
     Dim a As Range
     Dim DirName As String
-    DirName = "C:\DocReplace\TestSrc\"
+    'DirName = "C:\DocReplace\TestSrc\"
+    DirName = "C:\DocReplace\Old\"
     FileName = Dir(DirName & "*.doc")
     HeaderTemplateFileName = "C:\DocReplace\Template\PageHeader.doc"
     Set docTemplate = Documents.Open(HeaderTemplateFileName)
     Set hdrTemplate = docTemplate.Sections(1).Headers(wdHeaderFooterPrimary)
     Set hdrTemplate1st = docTemplate.Sections(1).Headers(wdHeaderFooterFirstPage)
+    On Error Resume Next
     Do While FileName <> ""
 
         FullName = DirName & FileName
@@ -21,7 +23,7 @@ Sub ChangeHeader()
         hdrTemplate.Range.Copy
         hdrDes.Range.Paste
         
-        doc.SaveAs2 FileName:="C:\DocReplace\TestDes\" & FileName
+        doc.SaveAs2 FileName:="C:\DocReplace\ReplaceHeaderFooter\" & FileName
     
         doc.Close
         Set doc = Nothing
@@ -31,4 +33,3 @@ Sub ChangeHeader()
     Loop
     
 End Sub
-
--- DocReplace/DocReplace/Program.cs
+++ DocReplace/DocReplace/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Word = Microsoft.Office.Interop.Word;
 namespace DocReplace
 {
@@ -16,11 +13,99 @@ namespace DocReplace
             Word._Document doc;
             app = new Word.Application();
             doc = app.Documents.Open(fullName);
-            string text = doc.Content.Text;
             string txtFileName = fullName.Replace(@".doc", @".txt");
-            File.WriteAllText(txtFileName, text);
+            //string text = doc.Content.Text;
+            //File.WriteAllText(txtFileName, text);
+            object formatAsObject = Word.WdSaveFormat.wdFormatText;
+            doc.SaveAs2(txtFileName, formatAsObject);
             app.Quit();
             Console.WriteLine(txtFileName);
+        }
+
+        static Regex regWJBM = new Regex(@"([^A-Z]+)([A-Z].+)", RegexOptions.Compiled);
+
+        //https://stackoverflow.com/questions/19252252/c-sharp-word-interop-find-and-replace-everything
+        static void FindAndReplace(Word.Application app, Word.Document doc, object findText, object replaceWithText)
+        {
+            //options
+            object matchCase = false;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+            //execute find and replace
+            foreach (Word.Range rng in doc.StoryRanges)
+            {
+                app.Selection.Find.Execute(ref findText, ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundsLike, ref matchAllWordForms, ref forward, ref wrap, ref format, ref replaceWithText, ref replace,
+                ref matchKashida, ref matchDiacritics, ref matchAlefHamza, ref matchControl);
+            }
+        }
+
+        static void SearchReplace(string fullName)
+        {
+            object oMissing = System.Reflection.Missing.Value;
+            Word.Application app;
+            Word.Document doc;
+            app = new Word.Application();
+            doc = app.Documents.Open(fullName);
+            string MixedName = Path.GetFileNameWithoutExtension(fullName).Trim().Replace("（包材）", "").Replace("（原料）", "").Replace("（辅料）", "");
+            string wjmc = regWJBM.Match(MixedName).Groups[1].Value;
+            string wjbm = regWJBM.Match(MixedName).Groups[2].Value;
+
+            //FindAndReplace(app, doc, "[WJMC]", wjmc);
+            //FindAndReplace(app, doc, "[WJBM]", wjbm);
+
+            foreach (Word.Section section in doc.Sections)
+            {
+                object missing = null;
+                object replaceAll = Word.WdReplace.wdReplaceAll;
+
+                //section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range.Text.Replace("[WJMC]", wjmc);
+                //section.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range.Text.Replace("[WJBM]", wjbm);
+                //section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterFirstPage].Range.Text.Replace("[WJMC]", wjmc);
+                //section.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterFirstPage].Range.Text.Replace("[WJBM]", wjbm);
+                Word.Range footerRange = section.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                footerRange.Find.Text = "[WJMC]";
+                footerRange.Find.Replacement.Text = wjmc;
+                footerRange.Find.Execute(ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref replaceAll, ref missing, ref missing, ref missing, ref missing);
+
+            }
+
+            //Word.Find findObject = app.Selection.Find;
+            //findObject.ClearFormatting();
+            //findObject.Text = "[WJMC]";
+            //findObject.Replacement.ClearFormatting();
+            //findObject.Replacement.Text = wjmc;
+
+            //object replaceAll = Word.WdReplace.wdReplaceAll;
+            //findObject.Execute(ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+            //    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+            //    ref replaceAll, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+            //Word.Find findObject1 = app.Selection.Find;
+            //findObject1.ClearFormatting();
+            //findObject1.Text = "[WJBM]";
+            //findObject1.Replacement.ClearFormatting();
+            //findObject1.Replacement.Text = wjbm;
+
+            //object replaceAll1 = Word.WdReplace.wdReplaceAll;
+            //findObject1.Execute(ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+            //    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+            //    ref replaceAll, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+            doc.Save();
+            app.Quit();
         }
 
         static void Main(string[] args)
@@ -29,6 +114,11 @@ namespace DocReplace
             {
                 SaveAsText(docFullName);
             }
+
+            //foreach (string docFullName in Directory.GetFiles(@"C:\DocReplace\TestDes", "*.doc"))
+            //{
+            //    SearchReplace(docFullName);
+            //}
             Console.WriteLine("ok");
             Console.ReadLine();
         }
