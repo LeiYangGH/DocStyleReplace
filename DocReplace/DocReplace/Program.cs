3d968c5cd8177de60a44304730229a26796a78d8
Lei, Yang <yang_lei@mcafee.com>
2017-08-09 09:10:09 +0800

    interop replace contents

--- DocReplace/DocReplace/Program.cs
+++ DocReplace/DocReplace/Program.cs
@@ -44,12 +44,12 @@ namespace DocReplace
             object replace = 2;
             object wrap = 1;
             //execute find and replace
-            foreach (Word.Range rng in doc.StoryRanges)
-            {
-                app.Selection.Find.Execute(ref findText, ref matchCase, ref matchWholeWord,
-                ref matchWildCards, ref matchSoundsLike, ref matchAllWordForms, ref forward, ref wrap, ref format, ref replaceWithText, ref replace,
-                ref matchKashida, ref matchDiacritics, ref matchAlefHamza, ref matchControl);
-            }
+            //foreach (Word.Range rng in doc.StoryRanges)
+            //{
+            app.Selection.Find.Execute(ref findText, ref matchCase, ref matchWholeWord,
+            ref matchWildCards, ref matchSoundsLike, ref matchAllWordForms, ref forward, ref wrap, ref format, ref replaceWithText, ref replace,
+            ref matchKashida, ref matchDiacritics, ref matchAlefHamza, ref matchControl);
+            //}
         }
 
         static void SearchReplace(string fullName)
@@ -59,28 +59,25 @@ namespace DocReplace
             Word.Document doc;
             app = new Word.Application();
             doc = app.Documents.Open(fullName);
-            string MixedName = Path.GetFileNameWithoutExtension(fullName).Trim().Replace("（包材）", "").Replace("（原料）", "").Replace("（辅料）", "");
-            string wjmc = regWJBM.Match(MixedName).Groups[1].Value;
-            string wjbm = regWJBM.Match(MixedName).Groups[2].Value;
+            //string MixedName = Path.GetFileNameWithoutExtension(fullName).Trim().Replace("（包材）", "").Replace("（原料）", "").Replace("（辅料）", "");
+            //string wjmc = regWJBM.Match(MixedName).Groups[1].Value;
+            //string wjbm = regWJBM.Match(MixedName).Groups[2].Value;
 
-            //FindAndReplace(app, doc, "[WJMC]", wjmc);
+            FindAndReplace(app, doc, "号", "**号**");
             //FindAndReplace(app, doc, "[WJBM]", wjbm);
 
-            foreach (Word.Section section in doc.Sections)
-            {
-                object missing = null;
-                object replaceAll = Word.WdReplace.wdReplaceAll;
+            //foreach (Word.Section section in doc.Sections)
+            //{
+            //    object missing = null;
+            //    object replaceAll = Word.WdReplace.wdReplaceAll;
 
-                //section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range.Text.Replace("[WJMC]", wjmc);
-                //section.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range.Text.Replace("[WJBM]", wjbm);
-                //section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterFirstPage].Range.Text.Replace("[WJMC]", wjmc);
-                //section.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterFirstPage].Range.Text.Replace("[WJBM]", wjbm);
-                Word.Range footerRange = section.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
-                footerRange.Find.Text = "[WJMC]";
-                footerRange.Find.Replacement.Text = wjmc;
-                footerRange.Find.Execute(ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref replaceAll, ref missing, ref missing, ref missing, ref missing);
 
-            }
+            //    Word.Range footerRange = section.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+            //    footerRange.Find.Text = "[WJMC]";
+            //    footerRange.Find.Replacement.Text = wjmc;
+            //    footerRange.Find.Execute(ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref replaceAll, ref missing, ref missing, ref missing, ref missing);
+
+            //}
 
             //Word.Find findObject = app.Selection.Find;
             //findObject.ClearFormatting();
@@ -111,15 +108,15 @@ namespace DocReplace
         static void Main(string[] args)
         {
             //foreach (string docFullName in Directory.GetFiles(@"C:\DocReplace\Old", "*.doc"))
-            foreach (string docFullName in Directory.GetFiles(@"C:\DocReplace\TestSrc", "*.doc"))
-            {
-                SaveAsText(docFullName);
-            }
-
-            //foreach (string docFullName in Directory.GetFiles(@"C:\DocReplace\TestDes", "*.doc"))
+            //foreach (string docFullName in Directory.GetFiles(@"C:\DocReplace\TestSrc", "*.doc"))
             //{
-            //    SearchReplace(docFullName);
+            //    SaveAsText(docFullName);
             //}
+
+            foreach (string docFullName in Directory.GetFiles(@"C:\DocReplace\TestDes", "*.doc"))
+            {
+                SearchReplace(docFullName);
+            }
             Console.WriteLine("ok");
             Console.ReadLine();
         }
